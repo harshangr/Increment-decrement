c8c42113b740b5e440ff9515e0d321e6c3db857e
harshang <ravalharshang8866@gmail.com>
2024-03-05 19:02:01 +0530

    Increment Decrement

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count,setCount] = useState(0);
+
+  function Increment(){
+    setCount((count + 1))
+  }
+
+  function Decrement(){
+    setCount((count - 1))
+  }
+
   return (
-    
+    <>
+    <div className="container">
+      <h1>{count}</h1>
+      <div className="btn-div">
+      <button onClick={Increment}>Increment</button>
+      <button onClick={Decrement}>Decrement</button>
+      </div>
+    </div>
+    </>
   )
 }
 
